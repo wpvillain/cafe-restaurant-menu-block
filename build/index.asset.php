a230d1af02d1568085b8f4e515e288c214b0c5f1
Jasper Frumau <jasper@imagewize.com>
2024-08-05 12:07:33 +0700

    InnerBlocks patch

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '57014c9d553c2d555797');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '57927969f63b7f67350c');
